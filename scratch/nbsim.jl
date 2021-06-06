87c3a524a7aaabd6b7486be493df8e5bc8fe9028
Neel Smith <dnsmith.neel@gmail.com>
2021-06-05 22:27:59 -0400

    Add scratch directory with nb simulator

---
+++ scratch/nbsim.jl
@@ -0,0 +1,46 @@
+# Simulate a validating notebook.
+using EditorsRepo
+using CitableText
+using CitableObject
+using ManuscriptOrthography
+using Orthography
+
+reporoot = pwd()
+repo = repository(reporoot)
+
+## CATALOGING CHECKS:
+cites = citation_df(repo)
+filesmatch(repo, cites)
+
+# ... document other functions listing discrepancies
+
+## SURFACES FOR MENU
+surfs = surfaces(repo)
+
+surface = surfs[1]
+
+
+
+
+
+## DSE RECORDS
+
+sdse = surfaceDse(repo, surface)
+# Use citableimage library to craft URLs to ICT2.
+
+## DSE ACCURACY
+
+row = sdse[1, :]
+diplnodetext(repo, row.passage)
+
+## ORTHOGRAPHY
+
+textconfig = citation_df(repo)
+
+tidy = EditorsRepo.baseurn(row.passage)
+
+# Get the orthography
+ortho = orthographyforurn(textconfig, tidy)
+chunks = normednodetext(repo, row.passage) |> split
+
+validstring(ortho, chunks[1])
